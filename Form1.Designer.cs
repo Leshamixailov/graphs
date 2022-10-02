2eaade795b92ac3391b09946016e3128b778f88f
Персик Сочный <rweerew@inbox.ru>
2022-10-03 01:20:43 +0300

    передижение по квадратам

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.createGraphToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -59,7 +61,7 @@
             // createGraphToolStripMenuItem
             // 
             this.createGraphToolStripMenuItem.Name = "createGraphToolStripMenuItem";
-            this.createGraphToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.createGraphToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
             this.createGraphToolStripMenuItem.Text = "Create graph";
             // 
             // pictureBox1
@@ -70,6 +72,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(1053, 551);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
             // 
             // pictureBox2
             // 
@@ -80,11 +83,31 @@
             this.pictureBox2.TabIndex = 4;
             this.pictureBox2.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1103, 575);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
@@ -107,6 +130,8 @@
         private System.Windows.Forms.ToolStripMenuItem createGraphToolStripMenuItem;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -42,5 +42,21 @@ namespace graphs
                 g.DrawEllipse(Blue,2,2,46,46);
             }
         }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Pen Green = new Pen(Color.Green, 1);
+            int x = e.X;
+            int y = e.Y;
+            label1.Text = x.ToString();
+            label2.Text = y.ToString();
+            int countX = x / 40;
+            int countY = y / 40;
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+            {
+                g.DrawRectangle(Green, countX * 40, countY * 40, 40, 40);
+            }
+        }
     }
 }
