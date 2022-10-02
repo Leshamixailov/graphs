f6c7436658cbfa9f43b4d1a0a97275619c0e5223
Персик Сочный <rweerew@inbox.ru>
2022-10-03 01:03:20 +0300

    создание сетки 40*40

--- Form1.cs
+++ Form1.cs
@@ -19,7 +19,21 @@ namespace graphs
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            Pen Green = new Pen(Color.Green, 1);
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            int countHorizontal = pictureBox1.Width / 40;
+            int countVertical = pictureBox1.Height / 40;
+            using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+            {
+                for (int i = 0; i < countHorizontal; i++)
+                {
+                    for (int j = 0; j < countVertical; j++)
+                    {
+                        g.DrawRectangle(Green, i * 40, j * 40, 40, 40);
+                    }
+                }
+            }
+            
             pictureBox2.Image = new Bitmap(pictureBox2.Width, pictureBox2.Height);
             Pen Blue = new Pen(Color.Blue,4);
             using (Graphics g = Graphics.FromImage(pictureBox2.Image))
